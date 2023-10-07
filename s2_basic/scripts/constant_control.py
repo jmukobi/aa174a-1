01472a5a16f139b6712805e6415b4bd4e5767b02
Adrian Gamarra <adriangamarra2018@gmail.com>
2023-10-07 11:54:45 -0700

    renaming to fit control

--- s2_basic/scripts/constant_control.py
+++ s2_basic/scripts/constant_control.py
@@ -6,41 +6,41 @@ from rclpy.node import Node
 from std_msgs.msg import Int64, Bool
 from geometry_msgs.msg import Twist
 
-class Heartbeat(Node):
+class Control(Node):
     def __init__(self) -> None:
-        super().__init__("heartbeat")
+        super().__init__("control")
 
-        self.hb_pub = self.create_publisher(Twist, "/cmd_vel", 10)
-        self.hb_timer = self.create_timer(1.0, self.hb_callback)
+        self.twist_pub = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.twist_timer = self.create_timer(1.0, self.twist_callback)
 
         self.twist_sub = self.create_subscription(Bool, "/kill",
                                                   self.kill_callback, 10)
-        self.hb_cnt = 0.0
+        self.twist_cnt = 0.0
 
-    def hb_callback(self) -> None:
+    def twist_callback(self) -> None:
         msg = Twist()
-        msg.angular.x = self.hb_cnt
-        msg.angular.z = self.hb_cnt
+        msg.angular.x = self.twist_cnt
+        msg.angular.z = self.twist_cnt
 
-        self.hb_pub.publish(msg)
+        self.twist_pub.publish(msg)
 
-        self.hb_cnt += 1.0
+        self.twist_cnt += 1.0
 
     def kill_callback(self, msg: Bool) -> None:
         print("called")
         self.get_logger().fatal("Control stopped")
         print("control stopped")
-        self.hb_timer.cancel()
+        self.twist_timer.cancel()
 
         msg = Twist()
         msg.angular.x = 0.0
         msg.angular.z = 0.0
 
-        self.hb_pub.publish(msg)
+        self.twist_pub.publish(msg)
             
 
 if __name__ == "__main__":
     rclpy.init()
-    node = Heartbeat()
+    node = Control()
     rclpy.spin(node)
     rclpy.shutdown()
